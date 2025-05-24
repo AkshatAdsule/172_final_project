16c4f41dc66df613c3db264a673072b5f66515b3
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2025-05-23 21:23:37 -0700

    basic websocket reciever

---
+++ frontend/src/hooks/useWS.tsx
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import { WSContext } from "../providers/ws";
+import type { UseWebSocketResult } from "../hooks/websocket";
+
+export function useWS(): UseWebSocketResult {
+	const context = useContext(WSContext);
+	if (!context) {
+		throw new Error("useWS must be used within a WSProvider");
+	}
+	return context;
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { WSProvider } from "./providers/ws.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// biome-ignore lint/style/noNonNullAssertion: Root element is guaranteed to exist
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<WSProvider url="ws://localhost:8080/ws">
+			<App />
+		</WSProvider>
+	</StrictMode>,
+);
