1548fb38b8d3f877d85aee5062e1cba60dbc5a44
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2025-05-24 02:15:50 -0700

    add rides path

--- frontend/src/routes/rides/index.tsx
+++ frontend/src/routes/rides/index.tsx
@@ -1,9 +1,19 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from "@tanstack/react-router";
+import "./styles/index.css";
 
-export const Route = createFileRoute('/rides/')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/rides/")({
+	component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <div>Hello "/rides/"!</div>
+	return (
+		<>
+			<div className="empty-state">
+				<span>
+					View <Link to="/rides">live location </Link>
+					or select a ride from your history.
+				</span>
+			</div>
+		</>
+	);
 }
