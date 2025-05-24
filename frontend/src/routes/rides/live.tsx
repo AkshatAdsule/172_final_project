158c1622d701f2ef9353f1ef94aa83a3cf1910fb
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2025-05-24 10:23:55 -0700

    refactor refactor

--- frontend/src/routes/rides/live.tsx
+++ frontend/src/routes/rides/live.tsx
@@ -1,9 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Map, useMap } from "@vis.gl/react-google-maps";
 import { useLatLng } from "../../hooks/useLatLng";
-import { useEffect } from "react";
-import { Polyline } from "./components/-polyline";
 import "./styles/live.css";
+import { MapComponent } from "../../components/MapComponent"; // Import the new component
 
 export const Route = createFileRoute("/rides/live")({
 	component: RouteComponent,
@@ -11,41 +9,10 @@ export const Route = createFileRoute("/rides/live")({
 
 function RouteComponent() {
 	const { latLngList } = useLatLng();
-	const map = useMap();
-
-	// setup polyline
-	useEffect(() => {
-		if (!map) {
-			if (map === undefined)
-				console.error("<Polyline> has to be inside a Map component.");
-
-			return;
-		}
-	}, [map]);
-
-	// calculate bounds to fit all lat lngs
-	useEffect(() => {
-		if (map && latLngList.length > 0) {
-			const bounds = new google.maps.LatLngBounds();
-			latLngList.forEach((latLng) => {
-				bounds.extend(new google.maps.LatLng(latLng.lat, latLng.lng));
-			});
-			map.fitBounds(bounds);
-		}
-	}, [map, latLngList]);
 
 	return (
 		<>
-			<Map
-				// for now center in sf
-				defaultCenter={{
-					lat: 37.774929,
-					lng: -122.419418,
-				}}
-				defaultZoom={17}
-			>
-				<Polyline path={latLngList} />
-			</Map>
+			<MapComponent latLngList={latLngList} />
 		</>
 	);
 }
