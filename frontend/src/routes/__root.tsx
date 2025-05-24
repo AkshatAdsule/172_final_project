bc26599601bd067f14468dd55e1bafa134faf304
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2025-05-24 12:54:43 -0700

    remove tanstack devtools

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,12 +1,10 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import "./root.css";
 
 export const Route = createRootRoute({
 	component: () => (
 		<>
 			<Outlet />
-			<TanStackRouterDevtools />
 		</>
 	),
 });
